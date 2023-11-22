2ae67f5a27f3ebfa067e219883a42193be30e13d
Ishaan <i.rawat98@gmail.com>
2023-11-22 14:17:14 +0000

    route created

--- backend/routes/countryRoutes.js
+++ backend/routes/countryRoutes.js
@@ -1,3 +1,7 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
+const countryController = require("../services/countryService");
 
+router.get('/:name', countryController.getCountryInfo);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express(); // Use express() to create an Express application
+const countryRoutes = require("./routes/countryRoutes");
 
 // Port Number
 const PORT = process.env.PORT || 5001;
+
+// middleware
+app.use(express.json());
+
+app.use("/country", countryRoutes);
 
 // Server Setup
 app.listen(PORT, () => {
