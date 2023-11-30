8594afed437eea93373690f65f7cecce38591e1c
Ishaan <i.rawat98@gmail.com>
2023-11-30 14:32:54 +0000

    sorted out dependencie issues

--- frontend/cypress/component/App.cy.jsx
+++ frontend/cypress/component/App.cy.jsx
@@ -1,7 +1,35 @@
-import App from '../../src/components/App';
+import React from "react";
+import App from "../../src/components/App";
 
-describe('App.cy.jsx', () => {
-  it('everything working as expected', () => {
-    cy.mount(< App/>)
-  })
-})
+describe("App Component", () => {
+  it("renders the component without crashing", () => {
+    cy.mount(<App />);
+    cy.contains("Enter a Country.");
+  });
+
+  it("displays error message for an invalid country name (numbers and symbols)", () => {
+    cy.mount(<App />);
+    cy.get("#search-bar").type("123"); // enter an invalid country name
+    cy.get("#button").click();
+    cy.contains("Please enter a valid country name.");
+  });
+
+  it("displays error message for incorrect country name", () => {
+    cy.mount(<App />);
+    cy.get("#search-bar").type("Irelan"); // enter an invalid country name
+    cy.get("#button").click();
+    cy.contains("Country not found, please try again.");
+  });
+
+  it("fetches country information for a valid country name", () => {
+    cy.mount(<App />);
+    cy.intercept("GET", "http://localhost:5001/country/*", {
+      fixture: "countryInfo.json",
+    }).as("countryInfo");
+
+    cy.get("#search-bar").type("Canada"); // enter a valid country name
+    cy.get("#button").click();
+    cy.wait("@countryInfo");
+    cy.get(".data-load").should("be.visible");
+  });
+});
